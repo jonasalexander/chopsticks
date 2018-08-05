13cc0bcea11ef8783334530d6923445b072c9c21
Jonas Nelle <jonas@nelle.de>
2018-08-04 21:00:03 -0500

    Add start parameter

--- interactive.py
+++ interactive.py
@@ -19,21 +19,50 @@ def main():
 	name = raw_input('Enter your name:\n')
 	b = InteractivePlayer(name=name)
 
+	start_str = raw_input('Should the computer start? (0 for no and 1 for yes)')
+	try:
+		start = int(start_str)
+		if start not in [0, 1]:
+			raise ValueError
+	except ValueError:
+		print 'Didn\'t get a 0 or 1 as expected for start_str question. Exiting'
+		return
+
 	while True:
-		a.turn(b)
+		if start == 1:
+			a.turn(b)
 
-		if not b.alive():
-			print b.name + ' loses'
-			break
+			if not b.alive():
+				print b.name + ' loses'
+				break
 
-		turn_success = b.turn(a)
+			turn_success = b.turn(a)
 
-		if not turn_success:
-			return
+			if not turn_success:
+				return
 
-		if not a.alive():
-			print 'Computer loses'
-			break
+			if not a.alive():
+				print 'Computer loses'
+				break
+		elif start == 0:
+			turn_success = b.turn(a)
+
+			if not turn_success:
+				return
+
+			if not a.alive():
+				print 'Computer loses'
+				break
+
+			a.turn(b)
+
+			if not b.alive():
+				print b.name + ' loses'
+				break
+
+	print 'Even if you just won, the computer got smarter soo...'
+	pickle.dump(a.model._attack_dict, open("attack_dict.pickle", "wb"))
+	pickle.dump(a.model._redistribution_dict, open("redistribution_dict.pickle", "wb"))
 
 if __name__ == '__main__':
 	main()
